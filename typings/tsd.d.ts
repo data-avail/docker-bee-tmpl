a4bef5c6d43f1e71da1ed1ff01c5577ddff6c673
baio <max.putilov@gmail.com>
2015-06-11 10:59:20 +0300

    fix

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="../trader-net/dist/definitions/tn.d.ts" />
 /// <reference path="../da-helpers/dist/index.d.ts" />
+/// <reference path="bluebird/bluebird.d.ts" />
